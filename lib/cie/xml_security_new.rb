b44199c6219999aa00dd49c15e4be3daaa0b7c92
fabiano <fabiano.pavan@soluzionipa.it>
2021-04-07 12:47:59 +0200

    Corretto metadata

--- lib/cie/xml_security_new.rb
+++ lib/cie/xml_security_new.rb
@@ -126,7 +126,7 @@ module Cie
         signed_info_element.add_element("ds:SignatureMethod", {"Algorithm"=>signature_method})
 
         # Add Reference
-        reference_element = signed_info_element.add_element("ds:Reference")
+        reference_element = signed_info_element.add_element("ds:Reference", {"URI" => ""})
 
         # Add Transforms
         transforms_element = reference_element.add_element("ds:Transforms")
